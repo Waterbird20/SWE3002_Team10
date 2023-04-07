e7cb424cd1fbf71b089c96b3a9107bd7ff2a0f09
leedc0101 <leedc0101@naver.com>
2023-04-07 10:02:32 +0900

    Add mobile navbar

--- front/src/component/Layout/Layout.tsx
+++ front/src/component/Layout/Layout.tsx
@@ -1,12 +1,26 @@
-import { Box, HStack, Flex, Spinner, useColorModeValue, VStack, Text, useMediaQuery } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import {
+  Box,
+  HStack,
+  Flex,
+  Spinner,
+  useColorModeValue,
+  VStack,
+  Text,
+  useMediaQuery,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from './Logo.png';
 import { Navbar } from '../Navbar/Navbar';
+import { MobileNavbarModal } from '../Navbar/MobileNavbarModal';
+import menu_burger from './menu-icon-dark.svg';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSm] = useMediaQuery('(max-width: 768px)');
+
   return (
     <Flex
       minW="100vw"
@@ -21,17 +35,28 @@ export const Layout = ({ children }: { children: ReactNode }) => {
       <VStack w="full" h="auto" maxW="1100px" px="20px" justify="space-between" spacing="50px">
         <HStack w="full" justify="space-between">
           <Link href="/">
-            <Image src={logo} alt="logo" />
+            <Image width={isSm ? 180 : 250} src={logo} alt="logo" />
           </Link>
-          <Navbar />
-          <HStack spacing="20px" fontSize="sm">
-            <Link href="/register">
-              <Text>회원가입</Text>
-            </Link>
-            <Link href="/login">
-              <Text>로그인</Text>
-            </Link>
-          </HStack>
+          {isSm ? (
+            <>
+              <MobileNavbarModal isOpen={isOpen} onClose={onClose} />
+              <Box _hover={{ cursor: 'pointer' }} onClick={onOpen}>
+                <Image width={20} src={menu_burger} alt="menu_burger" />
+              </Box>
+            </>
+          ) : (
+            <>
+              <Navbar />
+              <HStack spacing="20px" fontSize="sm">
+                <Link href="/register">
+                  <Text>회원가입</Text>
+                </Link>
+                <Link href="/login">
+                  <Text>로그인</Text>
+                </Link>
+              </HStack>
+            </>
+          )}
         </HStack>
 
         <VStack w="full" h="full" align="center">
---
+++ front/src/component/Navbar/MobileNavbarModal.tsx
@@ -0,0 +1,106 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  CloseButton,
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  Grid,
+  GridItem,
+  HStack,
+  ModalCloseButton,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../Layout/Logo.png';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export const MobileNavbarModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    onClose();
+  }, [router.route]);
+
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} placement="top">
+      <DrawerOverlay />
+      <DrawerContent
+        px={{ base: '20px', md: '50px' }}
+        py={{ base: '35px', md: '50px' }}
+        fontFamily="Montserrat"
+        maxH="100vh"
+        overflow="scroll"
+      >
+        <VStack w="full" align="flex-start" spacing="44px">
+          <HStack w="full" justify="space-between">
+            <Image src={logo} alt="logo" width={200} />
+            <CloseButton onClick={onClose} />
+          </HStack>
+          <Accordion w="full" allowToggle>
+            <AccordionItem py="10px">
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    <Text fontSize="lg" fontWeight={600} _hover={{ cursor: 'pointer' }}>
+                      모집과목
+                    </Text>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <VStack w="full" align="flex-start">
+                  <Link href="/class/tutee">
+                    <Text fontSize="lg" fontWeight={600}>
+                      튜티
+                    </Text>
+                  </Link>
+                  <Link href="/class/tutor">
+                    <Text fontSize="lg" fontWeight={600}>
+                      튜터
+                    </Text>
+                  </Link>
+                </VStack>
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem py="10px">
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    <Text fontSize="lg" fontWeight={600} _hover={{ cursor: 'pointer' }}>
+                      튜터
+                    </Text>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <VStack w="full" align="flex-start" spacing="20px">
+                  <Link href="/tutor/weekly_report">
+                    <Text fontSize="lg" fontWeight={500}>
+                      주간 보고서
+                    </Text>
+                  </Link>
+                  <Link href="/tutor/file_submission">
+                    <Text fontSize="lg" fontWeight={500}>
+                      서류 제출
+                    </Text>
+                  </Link>
+                </VStack>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </VStack>
+      </DrawerContent>
+    </Drawer>
+  );
+};
