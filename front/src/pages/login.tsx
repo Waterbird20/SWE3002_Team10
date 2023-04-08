08db5d6cdd97a210d81cb09987a2f82d9d3843d9
leedc0101 <leedc0101@naver.com>
2023-04-08 20:19:58 +0900

    Finish login & register

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,9 +1,38 @@
-import { Text, VStack } from '@chakra-ui/react';
+import { Logo } from '@/component/common/Logo';
+import { LockIcon } from '@chakra-ui/icons';
+import { Button, GenericAvatarIcon, HStack, Input, InputGroup, InputLeftAddon, Text, VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Login() {
   return (
-    <VStack>
-      <Text>Login</Text>
+    <VStack maxW="400px" w="full" h="full" align="center" justify="center" spacing="32px" pb="80px">
+      <Logo />
+      <VStack w="full" spacing="12px">
+        <InputGroup>
+          <InputLeftAddon
+            h="50px"
+            w="50px"
+            justifyContent="center"
+            p={0}
+            children={<GenericAvatarIcon boxSize="25px" color="black" />}
+          />
+          <Input h="50px" w="full" bg="white" textAlign="end" />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftAddon
+            h="50px"
+            w="50px"
+            justifyContent="center"
+            p={0}
+            children={<LockIcon boxSize="20px" color="black" />}
+          />
+          <Input h="50px" type="password" w="full" bg="white" textAlign="end" />
+        </InputGroup>
+      </VStack>
+      <Button h="50px" size="md" w="full" bg="#48702B" color="white">
+        로그인
+      </Button>
     </VStack>
   );
 }
--- front/src/pages/register.tsx
+++ front/src/pages/register.tsx
@@ -1,32 +1,37 @@
 import { Button, HStack, Input, InputGroup, InputLeftAddon, Text, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function Register() {
   return (
-    <VStack maxW="500px" w="full" h="full" align="flex-start" justify="center" spacing="32px" pb="80px">
+    <VStack maxW="400px" w="full" h="full" align="flex-start" justify="center" spacing="32px" pb="80px">
       <Text color="#76932F" fontSize="xl" fontWeight={600}>
         회원가입
       </Text>
       <VStack w="full" spacing="24px">
         <InputGroup>
           <InputLeftAddon children="학번" />
-          <Input type="number" w="full" bg="white" placeholder="학번" textAlign="end" />
+          <Input type="number" w="full" bg="white" textAlign="end" />
         </InputGroup>
         <InputGroup>
           <InputLeftAddon children="아이디" />
-          <Input w="full" bg="white" placeholder="아이디" textAlign="end" />
+          <Input w="full" bg="white" textAlign="end" />
         </InputGroup>
         <InputGroup>
           <InputLeftAddon children="비밀번호" />
-          <Input type="password" w="full" bg="white" placeholder="비밀번호" textAlign="end" />
+          <Input type="password" w="full" bg="white" textAlign="end" />
         </InputGroup>
         <InputGroup>
           <InputLeftAddon children="비밀번호" />
-          <Input type="password" w="full" bg="white" placeholder="비밀번호 확인" textAlign="end" />
+          <Input type="password" w="full" bg="white" textAlign="end" />
         </InputGroup>
       </VStack>
-      <HStack w="full">
-        <Button w="full">뒤로</Button>
-        <Button w="full">확인</Button>
+      <HStack w="full" color="white">
+        <Button w="full" bg="#8D8E8B">
+          뒤로
+        </Button>
+        <Button w="full" bg="#48702B">
+          확인
+        </Button>
       </HStack>
     </VStack>
   );
