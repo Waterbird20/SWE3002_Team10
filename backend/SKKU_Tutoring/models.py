988d8772d2b67d56472d4db579dfeecda89c366d
kothe <taehyun1018@gmail.com>
2023-05-05 11:49:22 +0900

    New table

--- backend/SKKU_Tutoring/models.py
+++ backend/SKKU_Tutoring/models.py
@@ -6,3 +6,10 @@ class Account(models.Model):
     email = models.CharField(max_length = 200)
     password = models.CharField(max_length = 200)
     # created_at = models.DateTimeField(auto_now_add=True)
+    
+class StudentId(models.Model):
+    email = models.CharField(max_length = 45)
+    name = models.CharField(max_length = 45)
+    student_id = models.IntegerField()
+    Tutor_Course_id = models.CharField(max_length=60)
+    Tutee_Course_id = models.CharField(max_length=60)
